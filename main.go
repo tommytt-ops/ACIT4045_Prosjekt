e73408bea400b064c984ba7f1d25e4c475e137fa
TommyT <tommytt721@gmail.com>
2024-08-29 19:26:00 +0200

    todo init

--- main.go
+++ main.go
@@ -20,12 +20,20 @@ func main() {
 	}
 
 	testHandler := func(w http.ResponseWriter, r *http.Request) {
-		templ := template.Must(template.ParseFiles("index.html"))
-
+		templ := template.Must(template.ParseFiles("frontend/index.html"))
 		templ.Execute(w, data)
-
 	}
+
+	addTodohandler := func(w http.ResponseWriter, r *http.Request) {
+		message := r.PostFormValue("message")
+		templ := template.Must(template.ParseFiles("frontend/index.html"))
+		todo := Todo{Id: len(data["Todos"]) + 1, Message: message}
+		data["Todos"] = append(data["Todos"], todo)
+		templ.ExecuteTemplate(w, "todo-list-element", todo)
+	}
+
 	http.HandleFunc("/", testHandler)
+	http.HandleFunc("/add-todo", addTodohandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
